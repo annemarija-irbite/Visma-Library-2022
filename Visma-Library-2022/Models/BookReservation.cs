771143eec4c50f4e74deaa5a21580b48d3e3d120
annemarija-irbite <annemarija.irbite@visma.com>
2022-03-07 10:52:56 +0200

    Added book reservations

---
+++ Visma-Library-2022/Models/BookReservation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Visma_Library_2022.Models
+{
+    public class BookReservation: IUser
+    {
+        public int ReservationId { get; set; }
+
+        [Key]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
