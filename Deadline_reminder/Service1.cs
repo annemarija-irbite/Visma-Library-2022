481c3e026d9878b23c0c6552c189a26e25de0c39
Jurģis Viļums <85902819+Vilumsy@users.noreply.github.com>
2022-03-07 12:02:21 +0200

    Emal_reminder
    Sends Email after specific time period

---
+++ Deadline_reminder/Service1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+namespace Deadline_reminder
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer(); // name space(using System.Timers;)  
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart(string[] args)
+        {
+
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds  
+            timer.Enabled = true;
+        }
+        protected override void OnStop()
+        {
+
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteEmail();
+        }
+        public void WriteEmail()
+        {
+
+
+            string to = "vilumsjurgis.lv@gmail.com"; //To address    
+            string from = "vilumsjurgis.lvv@gmail.com"; //From address    
+            MailMessage message = new MailMessage(from, to);
+
+            string mailbody = "In this article you will learn how to send a email using Asp.Net & C#";
+            message.Subject = "Sending Email Using Asp.Net & C#";
+            message.Body = mailbody;
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
+            System.Net.NetworkCredential basicCredential1 = new
+            System.Net.NetworkCredential("vilumsjurgis.lvv@gmail.com", "jv1345790862LVV");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = basicCredential1;
+
+
+
+            try
+            {
+
+                client.Send(message);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+
+        }
+    }
+}
