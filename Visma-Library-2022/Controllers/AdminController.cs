207c4de0aa8772d497a9007d9651b2cd840e17e6
alukonins <aleksejs.lukonins@visma.com>
2022-03-08 11:56:54 +0200

    add admin + user roles

---
+++ Visma-Library-2022/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Visma_Library_2022.Models;
+
+namespace Visma_Library_2022.Controllers
+{
+    [Authorize(Roles ="Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProjectRole role)
+        {
+            var roleExist = await roleManager.RoleExistsAsync(role.RoleName);
+            if (!roleExist)
+            {
+                var result = await roleManager.CreateAsync(new IdentityRole(role.RoleName));
+            }
+            return View();
+        }
+    }
+}
--- Visma-Library-2022/Data/ApplicationDbContext.cs
+++ Visma-Library-2022/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace Visma_Library_2022.Data
         }
         public DbSet<Visma_Library_2022.Models.Book> Book { get; set; }
         public DbSet<Visma_Library_2022.Models.BookReservation> BookReservation { get; set; }
+        public DbSet<Visma_Library_2022.Models.ProjectRole> ProjectRole { get; set; }
     }
 }
